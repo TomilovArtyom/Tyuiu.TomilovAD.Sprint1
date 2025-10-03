02bc82c5799457f6912dbef88cac627cb1e1d16c
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-10-03 23:19:00 +0500

    Task1

---
+++ Tyuiu.TomilovAD.Sprint1.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TomilovAD.Sprint1.Task1.V2.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint1.Task1.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 6.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint1.Task1.V2/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.TomilovAD.Sprint1.Task1.V2.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint1.Task0.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил Томилов А. Д. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ;                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле x*y/(5+x) и печатает его на экране.      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* x * y / (5 + x)                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+}
