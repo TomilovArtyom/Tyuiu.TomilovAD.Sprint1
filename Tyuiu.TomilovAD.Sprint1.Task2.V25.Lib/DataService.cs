a4ca244ae7cc060bcc817e85e2930452c0347436
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-10-15 23:02:22 +0500

    Task2

---
+++ Tyuiu.TomilovAD.Sprint1.Task2.V25.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Common;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TomilovAD.Sprint1.Task2.V25.Lib
+{
+    public class DataService : ISprint1Task2V25
+    {
+        public double ConvertRadsToDegrees(int value)
+        {
+            return Math.Round( value * 180 / Math.PI, 3);
+        }
+    }
+}
--- Tyuiu.TomilovAD.Sprint1.Task2.V25/Program.cs
+++ Tyuiu.TomilovAD.Sprint1.Task2.V25/Program.cs
@@ -1,6 +1,6 @@
-﻿using Tyuiu.TomilovAD.Sprint1.Task1.V2.Lib;
+﻿using Tyuiu.TomilovAD.Sprint1.Task2.V25.Lib;
 
-namespace Tyuiu.TomilovAD.Sprint1.Task1.V2
+namespace Tyuiu.TomilovAD.Sprint1.Task2.V25
 {
     class Program
     {
@@ -11,34 +11,31 @@ namespace Tyuiu.TomilovAD.Sprint1.Task1.V2
             Console.Title = "Спринт #1 | Выполнил Томилов А. Д. | ИСТНб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
-            Console.WriteLine("* Задание #1                                                              *");
-            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
             Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ;                                                                *");
             Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
-            Console.WriteLine("* вычисляет результат по формуле x*y/(5+x) и печатает его на экране.      *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* x * y / (5 + x)                                                         *");
+            Console.WriteLine("* x * 180 / π                                                             *");
             Console.WriteLine("***************************************************************************");
 
-            double x, y;
+            int x;
 
             Console.WriteLine("Введите значение X:");
-            x = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введите значение y:");
-            y = Convert.ToDouble(Console.ReadLine());
+            x = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine(ds.Calculate(x, y));
+            Console.WriteLine(ds.ConvertRadsToDegrees(x));
             Console.ReadLine();
         }
     }
