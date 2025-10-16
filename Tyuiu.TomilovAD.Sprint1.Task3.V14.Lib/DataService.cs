5f0566101d50078b9c86d0b296c90ee300fcab63
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-10-17 00:06:39 +0500

    Task 3

---
+++ Tyuiu.TomilovAD.Sprint1.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TomilovAD.Sprint1.Task3.V14.Lib
+{
+    public class DataService : ISprint1Task3V14
+    {
+        public double ReverseNumber(double number)
+        {
+            return (number % 100 - number % 10) + (number % 10 * 100) + ((number - (number % 100)) / 100);
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint1.Task3.V14/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.TomilovAD.Sprint1.Task3.V14.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint1.Task3.V14
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил Томилов А. Д. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ;                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        double x;
+
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.ReverseNumber(x));
+        Console.ReadLine();
+    }
+}
+}
